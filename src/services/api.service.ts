09713455d61af86bdaf05cc617bf524b2505e047
ZioCain <ziocain@hotmail.it>
2020-11-10 18:03:14 +0100

    Added iOS and Android

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 
 @Injectable({
 	providedIn: 'root'
@@ -8,7 +9,8 @@ import { Observable } from 'rxjs';
 export class APIService {
 	private readonly SERVER:string = "https://app.milk.it/";
 	constructor(
-		private http:HttpClient
+		private http:HttpClient,
+		private gb:Facebook
 	){}
 	public Login(email:string, password:string):Observable<any>{
 		return this.http.post(this.SERVER+"login",{
@@ -20,10 +22,13 @@ export class APIService {
 		return this.http.post('https://auth.apple.com/',{});
 	}
 	public LoginFacebook():Observable<any>{
-		return this.http.post('https://auth.apple.com/',{});
+		this.fb.login(['public_profile', 'email'])
+		.then((res:FacebookLoginResponse)=>console.log('logged into facebook', res))
+		.catch(e=>console.log('ERROR!',e));
+		return this.http.post(SERVER+'signin',{});
 	}
 	public LoginGoogle():Observable<any>{
-		return this.http.post('https://auth.apple.com/',{});
+		return this.http.post('https://auth.google.com/',{});
 	}
 	public GetOfferte():Observable<any>{
 		return this.http.get(this.SERVER+"offerte");
